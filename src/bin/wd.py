b394689279d8fc721fdd476a8fb529cc0435ca5b
Vicent Alabau <valabau@sciling.com>
2022-08-03 19:17:12 +0200

    Add support for wikipedia articles

--- src/bin/wd.py
+++ src/bin/wd.py
@@ -4,11 +4,13 @@ import re
 import sys
 import csv
 import json
+from queue import Queue
 from pathlib import Path
 from typing import List
 from urllib.parse import unquote
 
 import typer
+from tqdm import tqdm
 import wikipedia
 import wikipediaapi
 from wikipedia.exceptions import DisambiguationError, PageError
@@ -19,15 +21,15 @@ app = typer.Typer()
 
 
 wd_id_re = re.compile(r"^[PQL]\d+$")
+wp_pars_re = re.compile(r"\s*\(.*\)$")
 
 
-aliases = {
+wd_aliases = {
     'dish': 'Q746549',
     'food': 'Q2095',
     'sport': 'Q349',
     'feeling': 'Q9415',
 }
-
 
 query_template = """
 SELECT ?item ?itemLabel ?itemDescription ?lemma ?sitelink
@@ -45,13 +47,13 @@ WHERE {
 
 
 def wd_search(term: str, langs: str, wiki_lang: str = 'en'):
-    term = aliases.get(term, term)
+    term = wd_aliases.get(term, term)
     res = requests.get('https://query.wikidata.org/sparql', params={'format': 'json', 'query': query_template % (term, wiki_lang, langs)})
     return res.json().get('results', {}).get('bindings', [])
 
 
 @app.command()
-def search(terms: List[str], export_csv_fn: Path = typer.Option(None)):
+def search_wikidata(terms: List[str], export_csv_fn: Path = typer.Option(None)):
     typer.echo(f"Processing terms {terms} in to '{export_csv_fn}' ...")
 
     docs = {}
@@ -104,9 +106,80 @@ def search(terms: List[str], export_csv_fn: Path = typer.Option(None)):
         json.dump(docs, sys.stdout, indent=2, ensure_ascii=False)
 
 
+wp_aliases = {
+    'food': 'Categoría:Alimentos',
+    'sport': 'Categoría:Deportes',
+    'feeling': 'Categoría:Emociones',
+}
+
+
+def find_all_wp(terms: List[str]):
+    wiki = wikipediaapi.Wikipedia('es')
+
+    docs = {}
+    queued = set()
+    queue = Queue()
+
+    for term in terms:
+        term = wp_aliases.get(term, term)
+        if term not in queued:
+            queue.put(term)
+            queued.add(term)
+
+    with tqdm(total=queue.qsize()) as pbar:
+        while not queue.empty():  # and pbar.n < 10:
+            pbar.update()
+            term = queue.get()
+            if isinstance(term, str):
+                print(f"SEARCHING: {term}")
+                page = wiki.page(term)
+            else:
+                print(f"PROCESSING: {term.title}")
+                page = term
+                term = term.title
+
+            if page.exists():
+                if 'en' in page.langlinks:
+                    description = page.langlinks['en'].summary
+                else:
+                    description = ''
+
+                if description:
+                    docs[term] = {
+                        'label': wp_pars_re.sub('', term),
+                        'description': description,
+                    }
+
+                try:
+                    for member in page.categorymembers.values():
+                        if member not in queued:
+                            queue.put(member)
+                            queued.add(member)
+                    pbar.total = queue.qsize()
+                    pbar.refresh()
+                except KeyError:
+                    pass
+            else:
+                print(f"ERROR: page '{term}' does not exist")
+
+    return docs
+
+
 @app.command()
-def get_closest(csv_fn: Path, db_fn: Path):
-    pass
+def search(terms: List[str], export_csv_fn: Path = typer.Option(None)):
+    typer.echo(f"Processing terms {terms} in to '{export_csv_fn}' ...")
+    docs = find_all_wp(terms)
+
+    if export_csv_fn:
+        with open(export_csv_fn, "w") as file:
+            csvwriter = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for doc in docs.values():
+                if wd_id_re.match(doc['label']):
+                    continue
+                row = [doc['label'], doc['description']]
+                csvwriter.writerow(row)
+    else:
+        json.dump(docs, sys.stdout, indent=2, ensure_ascii=False)
 
 
 if __name__ == "__main__":
